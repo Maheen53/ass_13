e47569c690cce7703daee33770a79c3cd761b2b8
“[firstname <Maheen53@users.noreply.github.com>
2024-04-19 16:19:47 -0700

    ass_13

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+let transportationMode = ["car", "bike", "train", "airplain"];
+// for(let i= 0; i < transportationMode.length;i++ ){
+//     console.log(`i would like to owned my own ${transportationMode[1]} `)
+//
+// }
+transportationMode.forEach(mode => {
+    console.log(`i would likw to ow my owned ${mode[0]}`);
+});
